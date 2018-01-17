36560c101a4b01f7b3e40adea6dcd641ea8fe500
Trentham3269 <scottdalwood@gmail.com>
2018-01-17 15:00:20 +1100

    Add example ui inputs

--- ui.R
+++ ui.R
@@ -11,23 +11,53 @@ shinyUI(
   
       sidebarLayout(
         
-        sidebarPanel(width = 12,
+        sidebarPanel(width = 4,
       
           textInput(inputId = 'url',
-                    label   = 'Copy and paste web address of HEXTA plot to analyse'),
+                    label   = 'Copy and paste web address of HEXTA plot'),
           
-          selectInput(inputId = 'yards',
-                      label   = 'Choose yardage for MOA calc',
-                      choices = c(300, 400, 500, 600, 700, 800, 900, 1000),
-                      width   = '10%'),
+          dateInput(inputId = 'date',
+                    label   = 'Select date'),
+          
+          radioButtons(inputId = 'range',
+                       label   = 'Select imperial or metric distance',
+                       choices = c('Imperial', 'Metric'),
+                       inline  = TRUE),
+          
+            conditionalPanel(condition = "input.range == 'Imperial'",
+              selectInput(inputId = 'yards',
+                          label   = 'Select distance in yards',
+                          choices = c(300, 400, 500, 600, 700, 800, 900, 1000))
+            ),
+          
+            conditionalPanel(condition = "input.range == 'Metric'",
+              selectInput(inputId = 'metres',
+                         label    = 'Select distance in metres',
+                         choices  = c(300, 400, 500, 600, 700, 800, 900))
+            ),
+          
+          selectInput(inputId = 'barrel',
+                      label   = 'Select barrel',
+                      choices = c('Bartlein', 'Benchmark', 'Krieger', 'Maddco')), #TODO: Barrel ID
+          
+          selectInput(inputId = 'powder',
+                      label   = 'Select powder type',
+                      choices = c('BM8208', 'AR2206H', 'AR2208')),
+          
+          numericInput(inputId = 'weight',
+                       label   = 'Input powder weight (grains)',
+                       value   = 45.0,
+                       step    = 0.1),
   
           actionButton(inputId = 'analyse',
-                       label   = 'Analyse',
+                       label   = 'Analyse/Upload',
                        class   = 'btn-primary')
         
         ), 
         
         mainPanel(
+          
+          p('Custom stats to be calculated and uploaded to database')
           
         )
         
@@ -41,12 +71,22 @@ shinyUI(
       
       sidebarLayout(
         
-        sidebarPanel(width = 3
+        sidebarPanel(width = 4,
+                     
+          dateRangeInput(inputId = 'date_range',
+                         label   = 'Select date range'),
+          
+          actionButton(inputId = 'display',
+                       label   = 'Display',
+                       class   = 'btn-primary')
                      
         ),
     
         # Data Table
         mainPanel(
+          
+          p('Summary plots and statistics to be retrieved from database'),
+          
           verbatimTextOutput(outputId = 'txt'),
           br(),
           fluidRow(
