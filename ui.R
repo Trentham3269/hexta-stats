e09fd15488d005ded9eec55ce2f9ff372c2d6646
Trentham3269 <scottdalwood@gmail.com>
2018-01-17 14:20:41 +1100

    Change ui to navbar page

--- ui.R
+++ ui.R
@@ -1,53 +1,73 @@
 shinyUI( 
+    
+  # # Include Google Analytics
+  # tags$head(includeScript("www/google-analytics.js")),
+    
+  navbarPage(title = "HEXTA Stats",
+    
+    tabPanel(
+      title = 'Analyse/Upload',
+      id    = 'input',
   
-  fluidPage(
-    
-    # # Include Google Analytics
-    # tags$head(includeScript("www/google-analytics.js")),
-    
-    h2(titlePanel("HEXTA Stats Summary")),
-    br(),
-    
-    sidebarLayout(
+      sidebarLayout(
+        
+        sidebarPanel(width = 12,
       
-      sidebarPanel(width = 12,
-    
-        textInput(inputId = 'url',
-                  label   = 'Copy and paste web address of HEXTA plot to analyse'),
+          textInput(inputId = 'url',
+                    label   = 'Copy and paste web address of HEXTA plot to analyse'),
+          
+          selectInput(inputId = 'yards',
+                      label   = 'Choose yardage for MOA calc',
+                      choices = c(300, 400, 500, 600, 700, 800, 900, 1000),
+                      width   = '10%'),
+  
+          actionButton(inputId = 'analyse',
+                       label   = 'Analyse',
+                       class   = 'btn-primary')
         
-        selectInput(inputId = 'yards',
-                    label   = 'Choose yardage for MOA calc',
-                    choices = c(300, 400, 500, 600, 700, 800, 900, 1000),
-                    width   = '10%'),
-
-        actionButton(inputId = 'analyse',
-                     label   = 'Analyse',
-                     class   = 'btn-primary')
+        ), 
         
-      ),
+        mainPanel(
+          
+        )
+        
+      )
+        
+    ),
     
-      # Data Table
-      mainPanel(
-        verbatimTextOutput(outputId = 'txt'),
-        br(),
-        fluidRow(
-          column(width = 12,
-            withSpinner(
-              plotlyOutput(outputId = 'elev_plot'),
-              type = getOption("spinner.type", default = 7)
-            )
-          )
+    tabPanel(
+      title = 'Summary',
+      id    = 'output',
+      
+      sidebarLayout(
+        
+        sidebarPanel(width = 3
+                     
         ),
-        br(),
-        fluidRow(
-          column(width = 8,
-            tableOutput(outputId = 'tbl')
+    
+        # Data Table
+        mainPanel(
+          verbatimTextOutput(outputId = 'txt'),
+          br(),
+          fluidRow(
+            column(width = 12,
+              withSpinner(
+                plotlyOutput(outputId = 'elev_plot'),
+                type = getOption("spinner.type", default = 7)
+              )
+            )
           ),
-          column(width = 4,
-            tableOutput(outputId = 'tbl_misc')
+          br(),
+          fluidRow(
+            column(width = 8,
+              tableOutput(outputId = 'tbl')
+            ),
+            column(width = 4,
+              tableOutput(outputId = 'tbl_misc')
+            )
           )
         )
       )
     )
-  ) 
+  )
 )
